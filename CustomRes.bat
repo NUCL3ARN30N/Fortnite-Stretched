7543c977189df21bfcff3631a4e3bfa501dfaf29
NUCL3ARN30N <63464047+NUCL3ARN30N@users.noreply.github.com>
2021-04-26 11:37:08 +0200

    v1.1

--- CustomRes.bat
+++ CustomRes.bat
@@ -4,7 +4,7 @@ start com.epicgames.launcher://apps/Fortnite?action=launch&silent=true
 timeout /t 5 > NUL
 
 :loop
-tasklist /fi "IMAGENAME eq FortniteClient-Win64-Shipping_EAC.exe" | find ":" > nul
+tasklist /fi "IMAGENAME eq FortniteClient-Win64-Shipping_BE.exe" | find ":" > nul
 
 if errorlevel 1 (goto loop)
 
